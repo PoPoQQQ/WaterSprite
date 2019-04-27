4154b397d66c7fe85fae875d18c6047ea365d3f4
Fiiction <kai_jin_1998@163.com>
2019-04-27 22:01:39 +0800

    ,

--- Assets/Scripts/System/PlantManager.cs
+++ Assets/Scripts/System/PlantManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlantsManager : MonoBehaviour
+public class PlantManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
