5171b6a107cf1757125b83da0b66122e08af5a82
王天懿 <wty16@mails.tsinghua.edu.cn>
2019-04-28 00:58:09 +0800

    添加了击退和后摇无敌的效果。

--- Assets/Scripts/WaterSpriteBehavior.cs
+++ Assets/Scripts/WaterSpriteBehavior.cs
@@ -23,6 +23,25 @@ public class WaterSpriteBehavior : MonoBehaviour
         GetComponent<Rigidbody2D>().AddForce(dir * speed);
     }
 
+    IEnumerator FlashCoroutine()
+    {
+        const float duration = 0.5f;
+        const float frequency = 0.05f;
+        float _t = 0;
+
+        gameObject.layer = LayerMask.NameToLayer("Flash");
+        for(float t = 0; t < duration; t += Time.deltaTime)
+        {
+            _t += Time.deltaTime;
+            if(_t >= 2 * frequency)
+                _t -= 2 * frequency;
+            GetComponentInChildren<SpriteRenderer>().enabled = (_t >= frequency);
+            yield return 0;
+        }
+        GetComponentInChildren<SpriteRenderer>().enabled = true;
+        gameObject.layer = LayerMask.NameToLayer("Player");
+    }
+
     void OnCollisionEnter2D(Collision2D other)
     {
     	if(other.gameObject.name == "Enemy")
@@ -30,6 +49,7 @@ public class WaterSpriteBehavior : MonoBehaviour
     		Vector2 dir = (Vector2)(transform.position - other.gameObject.transform.position);
     		dir.Normalize();
     		GetComponent<Rigidbody2D>().AddForce(dir * 1200);
+            StartCoroutine(FlashCoroutine());
     	}
     }
 }
