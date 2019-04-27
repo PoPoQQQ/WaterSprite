f5df59f1a327b1e5d15451ca43dc79c29f7f5e01
王天懿 <wty16@mails.tsinghua.edu.cn>
2019-04-27 23:09:40 +0800

    添加了遮蔽的判断。

---
+++ Assets/Scripts/ZAxisAdjustion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZAxisAdjustion : MonoBehaviour
+{
+    const float minZAxis = 0f;
+    const float maxZAxis = 10f;
+    const float minYAxis = -100f;
+    const float maxYAxis = 100f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float y = transform.position.y;
+        float z = (y - minYAxis) / (maxYAxis - minYAxis) * (maxZAxis - minZAxis) + minZAxis;
+        transform.position = new Vector3(transform.position.x, transform.position.y, z);
+    }
+}
