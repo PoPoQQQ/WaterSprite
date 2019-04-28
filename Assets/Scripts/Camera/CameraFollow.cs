58110f4b3bf9331da58d1c30f918b4171588855f
王天懿 <wty16@mails.tsinghua.edu.cn>
2019-04-28 21:27:18 +0800

    更新美术素材，添加摄像机跟随效果。“

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+	public float followingSpeed = 5f;
+
+    GameObject player;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	Vector2 delta = player.transform.position - transform.position;
+        transform.Translate(new Vector3(delta.x, delta.y, 0) * followingSpeed * Time.deltaTime);
+    }
+}
