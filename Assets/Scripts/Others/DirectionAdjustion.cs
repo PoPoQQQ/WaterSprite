63c454e1f64334f0bf3d9b7f53470fdbb2aca8ce
王天懿 <wty16@mails.tsinghua.edu.cn>
2019-04-29 04:25:24 +0800

    添加了射击效果。

---
+++ Assets/Scripts/Others/DirectionAdjustion.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionAdjustion : MonoBehaviour
+{
+    public GameObject spriteCenter;
+
+    public void Adjust(float degree)
+    {
+    	Debug.Log(degree);
+    	if(-90 <= degree && degree < 90)
+    		spriteCenter.transform.eulerAngles = new Vector3(0, 0, degree);
+    	else
+    	{
+    		spriteCenter.transform.localScale = new Vector3(-1, 1, 1);
+    		spriteCenter.transform.eulerAngles = new Vector3(0, 0, degree + 180f);
+    	}
+    }
+}
---
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+	public float attackSpeed = 0.5f;
+    public float offsetY = 0f;
+
+    public GameObject bulletPrefab;
+
+    Coroutine c;
+
+    void Start()
+    {
+        
+    }
+
+    IEnumerator ShootingCoroutine()
+    {
+    	while(true)
+    	{
+    		Vector2 dir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        	dir.y -= offsetY;
+        	dir.Normalize();
+
+        	GameObject bullet = GameObject.Instantiate(bulletPrefab, transform.position, transform.rotation);
+        	bullet.GetComponent<Rigidbody2D>().AddForce(dir * 5f, ForceMode2D.Impulse);
+        	bullet.GetComponent<DirectionAdjustion>().Adjust(Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg);
+        	Destroy(bullet, 10f);
+
+        	yield return new WaitForSeconds(attackSpeed);
+    	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        	c = StartCoroutine(ShootingCoroutine());
+        if(Input.GetMouseButtonUp(0))
+        	StopCoroutine(c);
+    }
+}
