dc391fdcdba425ae2c4187d990d503240cd77c48
Fiiction <kai_jin_1998@163.com>
2019-04-28 23:29:56 +0800

    enemy behaviours modified

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    public float health = 10F;
     public float collideDamage = 5F;
     public float collideKnockBack = 24F;
     // Start is called before the first frame update
@@ -17,6 +18,30 @@ public class EnemyController : MonoBehaviour
     {
         
     }
+
+    IEnumerator FlashCoroutine()
+    {
+        const float duration = 0.5f;
+        const float frequency = 0.05f;
+        float _t = 0;
+
+        for (float t = 0; t < duration; t += Time.deltaTime)
+        {
+            _t += Time.deltaTime;
+            if (_t >= 2 * frequency)
+                _t -= 2 * frequency;
+            GetComponentInChildren<SpriteRenderer>().enabled = (_t >= frequency);
+            yield return 0;
+        }
+        GetComponentInChildren<SpriteRenderer>().enabled = true;
+    }
+    void Damage(float damage)
+    {
+        health -= damage;
+        StartCoroutine(FlashCoroutine());
+        Destroy(gameObject);
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Player")
