012ab6b02258cb3e10afab22e58c05bba3550ebf
Fiiction <kai_jin_1998@163.com>
2019-04-28 17:35:12 +0800

    plants

---
+++ Assets/Scripts/UI/PlantUIButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantUIButton : MonoBehaviour
+{
+    public PlantUIController.UISelectStatus type;
+    public Plant.Type seedType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        PlantUIController.uiSelectStatus = type;
+        PlantUIController.selectedSeedType = seedType;
+    }
+}
