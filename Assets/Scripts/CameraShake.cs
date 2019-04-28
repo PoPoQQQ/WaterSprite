00d959e9aa6ef97472bf2ac90bfa447d48852c0d
王天懿 <wty16@mails.tsinghua.edu.cn>
2019-04-28 14:45:06 +0800

    修正抖动效果。

--- Assets/Scripts/CameraShake.cs
+++ Assets/Scripts/CameraShake.cs
@@ -5,8 +5,13 @@ using DG.Tweening;
 
 public class CameraShake : MonoBehaviour
 {
+	public float duration = 0.5f;
+	public float strength = 0.08f;
+	public int time = 20;
+	public float randomness = 90f;
+
     public void Shake()
     {
-    	transform.DOShakePosition(1f, 0.8f, 10, 10f);
+    	transform.DOShakePosition(duration, strength, time, randomness);
     }
 }
