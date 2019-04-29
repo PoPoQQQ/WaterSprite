15c804263b3c2f2602c5c508c2fc0302ea5e0579
王天懿 <wty16@mails.tsinghua.edu.cn>
2019-04-30 07:17:15 +0800

    设置分辨率

--- Assets/Scripts/System/GameSystem.cs
+++ Assets/Scripts/System/GameSystem.cs
@@ -95,6 +95,7 @@ public class GameSystem : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Screen.SetResolution(Screen.width, Screen.width / 16 * 9, Screen.fullScreen);
         dayNightManager.setDayCnt(1);
         DOTween.Init();
         plants = FindObjectsOfType<Plant>();
