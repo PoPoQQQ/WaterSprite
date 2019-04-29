df677b5cf12d66466be180aa3f535b5075745deb
Fiiction <kai_jin_1998@163.com>
2019-04-29 18:47:03 +0800

    plant type modified

--- Assets/Scripts/Player/PlayerPlantOperate.cs
+++ Assets/Scripts/Player/PlayerPlantOperate.cs
@@ -5,17 +5,29 @@ using UnityEngine;
 public class PlayerPlantOperate : MonoBehaviour
 {
     public Dictionary<Plant.Type, int> seedCnt;
-
+    float plantCost = 30F;
+    Player pl;
     // Start is called before the first frame update
     void Start()
     {
         seedCnt = new Dictionary<Plant.Type, int>();
         seedCnt.Add(Plant.Type.Water, 0);
-        seedCnt.Add(Plant.Type.Buff, 0);
-        seedCnt.Add(Plant.Type.Ultimate, 0);
-        seedCnt[Plant.Type.Ultimate]++;
+        seedCnt.Add(Plant.Type.Attack, 0);
+        seedCnt.Add(Plant.Type.Consume, 0);
+        seedCnt[Plant.Type.Water]++;
+        pl = GetComponent<Player>();
     }
-
+    public void TryPlant(Plant plant, Plant.Type seedType)
+    {
+        if (!seedCnt.ContainsKey(seedType))
+            return;
+        if (seedCnt[seedType] <= 0)
+            return;
+        if (pl.health <= plantCost)
+            return;
+        pl.CostHealth(plantCost);
+        plant.PlantSeed(seedType);
+    }
     // Update is called once per frame
     void Update()
     {
