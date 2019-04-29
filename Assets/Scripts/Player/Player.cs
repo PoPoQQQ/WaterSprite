80665add5264adfc4e39acea8c21f8a1554b746a
煌瞳小妖 <892611452@qq.com>
2019-04-30 01:21:27 +0800

    UI更新

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -14,6 +14,7 @@ public class Player : MonoBehaviour
     public int atkBuffCnt = 0;
     public int csmBuffCnt = 0;
     public HPBar bar;
+    public ScrollBar scrollBar;
     
 
     void GameOver()
