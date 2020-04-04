c86ef83a6d5003f733c5a81fd2c18927bf786dc0
MitoGame <flameyeelf@gmail.com>
2020-04-04 15:17:37 +0800

    plant animation

---
+++ Assets/Scripts/Plant/PlantAnimateLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantAnimateLoader : MonoBehaviour
+{
+    Animator anim;
+    AnimatorOverrideController overrideController;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        overrideController = new AnimatorOverrideController();
+        overrideController.runtimeAnimatorController = anim.runtimeAnimatorController;
+
+    }
+
+    public void loadAnimation(Plant.Type seedType)
+    {
+        CollecableSeed temp = ScriptableObject.CreateInstance("CollecableSeed") as CollecableSeed;
+        temp = Resources.Load<CollecableSeed>(CollecableSeed.seedDictionary[seedType]);
+        string path = "PlantAnimate/" + temp.name;
+        AnimationClip ani = Resources.Load<AnimationClip>(path);
+        overrideController["Aquabud"] = ani;
+        anim.runtimeAnimatorController = overrideController;
+    }
+}
